9f9af2d6a4a49601796530dee9f427338b1394f1
Sehee <sehuii152@gmail.com>
2025-08-14 14:18:43 +0900

    node modules 삭제

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const { createTask, getAllTasks, updateTask, deleteTask } = require('../controllers/taskController');
 
-router.post('/tasks', createTask); // POST /tasks
-router.get('/tasks', getAllTasks); 
-router.put('/tasks/:id', updateTask);    // 업무 수정
-router.delete('/tasks/:id', deleteTask); // 업무 삭제
+router.post('/', createTask); // POST /tasks
+router.get('/', getAllTasks); 
+router.put('/:id', updateTask);    // 업무 수정
+router.delete('/:id', deleteTask); // 업무 삭제
 
 module.exports = router;
